e10d2cbf8225c7da06a58d5b46e50cee5dbce157
Matheus Alves <theuves@outlook.com>
2018-03-17 20:07:55 -0400

    melhora o uso do css

--- app/directives/aguDragAndDrop.js
+++ app/directives/aguDragAndDrop.js
@@ -8,33 +8,33 @@ function aguDragAndDrop() {
     },
     link: (scope, element, attrs) => {
       const text = angular
-        .element(`<span class="text"></span>`)
+        .element(`<span class="dragAndDrop__text"></span>`)
         .text(attrs.aguMsgDefault);
 
       element.append(text);
 
       element.on("dragover", event => {
         event.preventDefault()
-        element.addClass("agu-active");
+        element.addClass("dragAndDrop--active");
         text.text(attrs.aguMsgDragover);
       });
 
       element.on("dragleave", event => {
         event.preventDefault()
-        element.removeClass("agu-active");
+        element.removeClass("dragAndDrop--active");
         text.text(attrs.aguMsgDefault);
       });
 
       element.on("drop", event => {
         event.preventDefault()
-        element.addClass("cur-progress");
-        element.removeClass("agu-active");
+        element.addClass("cur--progress");
+        element.removeClass("dragAndDrop--active");
         text.text(attrs.aguMsgOnDrop);
 
         const isOk = scope.aguOnDrop(event);
 
         if (!isOk) {
-          element.removeClass("cur-progress");
+          element.removeClass("cur--progress");
           text.text(attrs.aguMsgDefault);
         }
 
