af341ae31228f2b80292cc105d8926dc9dd71d36
Matheus Alves <theuves@outlook.com>
2018-03-17 20:39:56 -0400

    corrige o funcionamento do filtro de dados

--- app/filters/search.js
+++ app/filters/search.js
@@ -15,20 +15,19 @@ function search(
     const hasMainFilter = isNumber(start1) && isNumber(end1) && start1 <= end1;
     const hasAdditionalFilter = isNumber(start2) && isNumber(end2) && start2 <= end2;
 
-    if (hasMainFilter) {
-      return data.filter(doc => {
-        const processo = doc.processo;
-        const number = processo.replace(/^.*>(\d+)<.*$/, "$1");
-        const condition1 = start1 <= number && end1 >= number;
-        const condition2 = start2 <= number && end2 >= number;
+    if (!hasMainFilter && !hasAdditionalFilter) return data;
 
-        return hasAdditionalFilter
-          ? condition1 || condition2
-          : condition1;
-      });
-    } else {
-      return data;
-    }
+    return data.filter(doc => {
+      const processo = doc.processo;
+      const number = processo.replace(/^.*>(\d+)<.*$/, "$1");
+      const condition1 = start1 <= number && end1 >= number;
+      const condition2 = start2 <= number && end2 >= number;
+
+      if (hasMainFilter && hasAdditionalFilter) return condition1 || condition2;
+      if (hasMainFilter) return condition1;
+
+      return condition2;
+    });
   };
 }
 
